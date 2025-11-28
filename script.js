6f697af5c756bb9d21a65f1d9fe5776019d18681
to3-12a2 <mingskibidi1@gmail.com>
2025-11-28 16:33:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const taskList = document.getElementById('taskList');
+
+  function showTasks() {
+    taskList.style.display = 'block';
+  }
+
+  function closeTasks() {
+    taskList.style.display = 'none';
+  }
